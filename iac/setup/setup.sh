056bf033910c6efaba38b6a5e9e656f12589f065
Leonardo Pinho <leo.o.nardo.sant@gmail.com>
2024-01-06 04:35:53 -0300

    ci: add dockerfile and docker build config

--- iac/setup/setup.sh
+++ iac/setup/setup.sh
@@ -1,7 +1,15 @@
+# THIS SCRIPT RUNS ON HOST MACHINE (NOT INSIDE CONTAINER)
 sudo apt update
 sudo apt install docker.io -y
 USERNAME=$(whoami)
 sudo usermod -aG docker "$USERNAME"
 newgrp docker
-docker run --name "jenkins" -p 8080:8080 -p 50000:50000 -d -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts
-docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword
+docker run --name "jenkins" -p 8080:8080 -p 50000:50000 -d  \
+-v jenkins_home:/var/jenkins_home \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v $(which docker):/usr/bin/docker \
+jenkins/jenkins:lts
+## to jenkins user have permission to use docker on pipelines
+chmod 666 /var/run/docker.sock
+
+#docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword
